155328ffd54e1f9ae6a941d5a9c1cd3d91f81760
Vijay Vasu <vijayvasuv@gmail.com>
2025-04-16 21:09:53 -0500

    Initial commit
    Program to filter results from a list based on the input

---
+++ List Filters/listfilters.py
@@ -0,0 +1,52 @@
+class list:
+    def __init__(self, list):
+        self.list = list
+        self.result = []
+
+    def even(self):
+        for i in self.list:
+            if isinstance(i, int):
+                if i % 2 == 0:
+                    self.result.append(i)
+        return self.result
+
+    def odd(self):
+        for i in self.list:
+            if isinstance(i, int):
+                if i % 2 != 0:
+                    self.result.append(i)
+        return self.result
+
+    def under(self, num):
+        self.num = num
+        for i in self.list:
+            if isinstance(i, int):
+                if i < num:
+                    self.result.append(i)
+        return self.result
+
+
+    def over(self, num):
+        self.num = num
+        for i in self.list:
+            if isinstance(i, int):
+                if i > num:
+                    self.result.append(i)
+        return self.result
+
+    def in_range(self, start, end):
+        self.start = start
+        self.end = end
+        for i in self.list:
+            if isinstance(i, int):
+                if start <= i <= end:
+                    self.result.append(i)
+        return self.result
+
+
+def main():
+    new_list = list([1,2,3,4,5,6])
+    print((new_list.even()).under(5))
+
+if __name__ == "__main__":
+    main()
